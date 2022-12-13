bcf124cacb16f2235e21d1e199198975067107cd
Yasir <yasir.ibrahim@hotmail.co.uk>
2022-12-13 17:46:39 +0000

    DBFirst... refactoring completed

--- DBFirst_3Layer_TestStarterCode/NorthwindBusiness/CustomerManager.cs
+++ DBFirst_3Layer_TestStarterCode/NorthwindBusiness/CustomerManager.cs
@@ -1,82 +1,63 @@
-﻿using System;
+﻿using NorthwindData;
+using NorthwindData.Services;
+using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Diagnostics;
-using NorthwindData;
 
-namespace NorthwindBusiness
-{
-    public class CustomerManager
-    {
+namespace NorthwindBusiness {
+    public class CustomerManager {
+        private ICustomerService _service;
+        public CustomerManager() {
+            _service = new CustomerService();
+        }
+
+        public CustomerManager( ICustomerService service ) {
+            if ( service == null ) {
+                throw new ArgumentException( "ICustomerService object cannot be null" );
+            }
+            _service = service;
+        }
+
         public Customer SelectedCustomer { get; set; }
 
-        public void SetSelectedCustomer(object selectedItem)
-        {
-            SelectedCustomer = (Customer)selectedItem;
+        public void SetSelectedCustomer( object selectedItem ) {
+            SelectedCustomer = ( Customer ) selectedItem;
         }
 
-        public List<Customer> RetrieveAll()
-        {
-            using (var db = new NorthwindContext())
-            {
-                return db.Customers.ToList();
-            }
+        public List<Customer> RetrieveAll() {
+            return _service.GetCustomerList();
         }
 
-        public void Create(string customerId, string contactName, string companyName, string city = null)
-        {
+        public void Create( string customerId, string contactName, string companyName, string city = null ) {
+
             var newCust = new Customer() { CustomerId = customerId, ContactName = contactName, CompanyName = companyName, City = city };
-            using (var db = new NorthwindContext())
-            {
-                db.Customers.Add(newCust);
-                db.SaveChanges();
+            _service.CreateCustomer(
+            new Customer() {
+                CustomerId = customerId,
+                ContactName = contactName,
+                CompanyName = companyName,
             }
+            );
         }
 
-        public bool Update(string customerId, string contactName, string country, string city, string postcode)
-        {
-            using (var db = new NorthwindContext())
-            {
-                var customer = db.Customers.Where(c => c.CustomerId == customerId).FirstOrDefault();
-                if (customer == null)
-                {
-                    Debug.WriteLine($"Customer {customerId} not found");
-                    return false;
-                }
-                customer.ContactName = contactName;
-                customer.City = city;
-                customer.PostalCode = postcode;
-                customer.Country = country;
-                // write changes to database
-                try
-                {
-                    db.SaveChanges();
-                    SelectedCustomer = customer;
-                }
-                catch (Exception e)
-                {
-                    Debug.WriteLine($"Error updating {customerId}");
-                    return false;
-                }
+        public bool Update( string customerId, string contactName, string country, string city, string postcode ) {
+            var customer = _service.GetCustomerById( customerId );
+            if ( customer == null ) {
+                Debug.WriteLine( $"Customer {customerId} not found" );
+                return false;
             }
+            customer.ContactName = contactName;
+            customer.City = city;
+            customer.PostalCode = postcode;
+            customer.Country = country;
+            _service.SaveCustomerChanges();
+            SelectedCustomer = customer;
             return true;
         }
 
-        public bool Delete(string customerId)
-        {
-            using (var db = new NorthwindContext())
-            {
-                var customer = db.Customers.Where(c => c.CustomerId == customerId).FirstOrDefault();
-                if (customer == null)
-                {
-                    Debug.WriteLine($"Customer {customerId} not found");
-                    return false;
-                }
-                db.Customers.Remove(customer);
-                SelectedCustomer = null;
-                db.SaveChanges();
-            }
-            return true;
+        public void Delete( string customerId ) {
+            var cust = _service.GetCustomerById( customerId );
+            _service.RemoveCustomer( cust );
         }
     }
 }
